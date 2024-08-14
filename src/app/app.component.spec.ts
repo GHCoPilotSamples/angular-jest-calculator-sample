707f89a6ba98105443c0133bc98a3e4394fce24e
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2024-08-14 17:22:59 +0000

    Karma module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,18 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent]
+    await TestBed.configureTestingModule({
+     imports: [AppComponent],
+    }).compileComponents();
     });
-    const fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
   });
-
-  it('should subtract two numbers', () => {
-    const result = component.subtract(5, 3);
-    expect(result).toBe(2);
-  });
-});
