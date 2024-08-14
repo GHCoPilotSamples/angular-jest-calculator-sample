10ace0a7526f5027f87559a5ca91f880016cdeed
Basujita B <Basujitab@microsoft.com>
2024-08-14 13:07:49 -0400

    Karma tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,19 @@
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let fixture: AppComponent;
+  let component: AppComponent;
+
   beforeEach(() => {
-    fixture = new AppComponent();
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+    const fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-  
+
+  it('should subtract two numbers', () => {
+    const result = component.subtract(5, 3);
+    expect(result).toBe(2);
+  });
 });
