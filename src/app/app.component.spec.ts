7ea30f27ca5663a8597890723866ccb8a02138f6
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2024-08-14 16:00:41 +0000

    Cleaning jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,25 +5,5 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = new AppComponent();
   });
-  it('add two numbers', () => {
-    expect(fixture.sum(1,4)).toBe(5);
-  });
-  //write test for all the methods subtract, multiply, divide, fibonacci, square
-  it('subtract two numbers', () => {
-    expect(fixture.subtract(4,1)).toBe(3);
-  });
-  it('multiply two numbers', () => {
-    expect(fixture.multiply(4,1)).toBe(4);
-  });
-  it('divide two numbers', () => {
-    expect(fixture.divide(4,1)).toBe(4);
-  });
-  it('fibonacci of a number', () => {
-    expect(fixture.fibonacci(4)).toBe(3);
-  });
-  it('square of a number', () => {
-    expect(fixture.square(4)).toBe(16);
-  });
   
-
 });
